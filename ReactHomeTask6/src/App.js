b6194a4d676587ca76299de69feae2732af74201
unknown <ali.zaidov.18@gmail.com>
2023-08-31 12:01:45 +0400

    Done

---
+++ ReactHomeTask6/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import OperationComponent from "./OperationComponent";
+
+function App() {
+  return (
+    <div style={{ backgroundColor: "#383838" }} className="App">
+      <OperationComponent></OperationComponent>
+      {/* <AllCars></AllCars> */}
+    </div>
+  );
+}
+
+export default App;
